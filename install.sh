88b39a49aabec477c2908493bc9fbc1d22813777
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-03-26 15:20:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-# Install required packages
-apt-get update && apt-get install -y nginx python3-pip python3 git
+# Update package list
+apt-get update
 
-# Clone repository and install dependencies
-git clone https://github.com/glenn-sorrentino/pgp-signup-form.git
-cd pgp-signup-form
+# Install dependencies
+apt-get install -y nginx python3-pip python3 git ufw
+
+# Clone repository
+REPO_NAME="pgp-signup-form"
+mkdir $REPO_NAME
+git clone https://github.com/glenn-sorrentino/$REPO_NAME.git $REPO_NAME
+
+# Create virtual environment and install dependencies
+cd $REPO_NAME
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-# Configure nginx
-cat <<EOF >/etc/nginx/sites-available/pgp-signup-form
-server {
-    listen 80;
-    server_name _;
+# Configure Nginx
+cp nginx_config /etc/nginx/sites-available/$REPO_NAME
+ln -s /etc/nginx/sites-available/$REPO_NAME /etc/nginx/sites-enabled/$REPO_NAME
+sed -i 's/server_name example.com;/server_name localhost;/g' /etc/nginx/sites-available/$REPO_NAME
 
-    location / {
-        proxy_pass http://localhost:5000;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    }
-}
-EOF
-ln -sf /etc/nginx/sites-available/pgp-signup-form /etc/nginx/sites-enabled/pgp-signup-form
-nginx -t && systemctl restart nginx
+# Start Nginx
+systemctl restart nginx
 
-# Setup firewall
+# Configure firewall
 ufw allow 'Nginx Full'
-
-echo "Installation complete."
+ufw allow 'OpenSSH'
+ufw enable
